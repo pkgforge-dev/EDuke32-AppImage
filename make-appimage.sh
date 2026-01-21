81323dcc4fd61490d17ea27116b07669e9ae9685
Link4Electronics <link4electronics@gmail.com>
2026-01-20 23:48:10 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,13 +8,13 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=source/duke3d/rsrc/game_icon.ico
+export ICON=/src/eduke32_20251111-10652-39967d866/duke3d/rsrc/game_icon.ico
 export DESKTOP=eduke32.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun eduke32
+quick-sharun /src/eduke32_20251111-10652-39967d866/eduke32
 
 # Additional changes can be done in between here
 

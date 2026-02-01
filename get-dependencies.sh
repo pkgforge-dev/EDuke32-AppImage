217021fbac123bf349d9610e2d1d6fc2a464de26
Link4Electronics <link4electronics@gmail.com>
2026-02-01 12:07:03 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,19 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm glu
+pacman -Syu --noconfirm \
+    alsa-lib  \
+    flac      \
+    glu       \
+    libvorbis \
+    libvpx
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package gtk2
+make-aur-package gtk2 sdl2
 make-aur-package
 
 # If the application needs to be manually built that has to be done down here

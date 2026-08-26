325ba44c1d313ebd7c3be508ebb6a53e91bbc4c7
Link4Electronics <link4electronics@gmail.com>
2026-08-26 16:08:55 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/eduke32
+quick-sharun ./AppDir/bin/eduke32
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
